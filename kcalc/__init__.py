2395b192aceaf27bc2f9606ca8b1a81f2b206cf3
finlayclark <s1629668@ed.ac.uk>
2024-08-27 14:51:10 +0100

    Update versioning

--- kcalc/__init__.py
+++ kcalc/__init__.py
@@ -1,7 +1,6 @@
 """Tutorial"""
 
 from . import _version
-from .conversion import calc_k
 
 __version__ = _version.get_versions()["version"]
-__all__ = ["__version__", "calc_k"]
+__all__ = ["__version__"]
