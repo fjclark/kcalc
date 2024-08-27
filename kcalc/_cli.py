3e1e45af2d502cc355520012bf6e5fe4397ed249
finlayclark <s1629668@ed.ac.uk>
2024-08-27 15:29:59 +0100

    Update docs more

--- kcalc/_cli.py
+++ kcalc/_cli.py
@@ -6,14 +6,17 @@ from .conversion import calc_k
 
 
 @click.command
-@click.argument("dg", type=float)
+@click.argument("neg_dg", type=float)
 @click.option(
     "--temp",
     type=float,
     default=298.15,
     help="Temperature in Kelvin. Default is 298.15 K.",
 )
-def main(dg: float, temp: float):
-    """Calculate Kd from a standard free energy of binding (in kcal/mol)."""
-    kd = calc_k(dg, temp)
+def main(neg_dg: float, temp: float):
+    """
+    Calculate Kd from a standard free energy of binding (in kcal/mol).
+    The standard free energy of binding should be entered as its negative
+    (e.g. enter -10 kcal /mol as '10')."""
+    kd = calc_k(-neg_dg, temp)
     print(f"Kd = {kd:.2e} M")
--- kcalc/conversion.py
+++ kcalc/conversion.py
@@ -1,4 +1,4 @@
-"""Calculate Kd from a standard free energy of binding."""
+"""Functions for converting between free energy and dissociation constant."""
 
 import numpy as _np
 from scipy import constants as _const
@@ -6,28 +6,23 @@ from scipy import constants as _const
 
 def calc_k(dg: float, temp: float = 298.15) -> float:
     """
-    Calculate Kd from a standard free energy of binding.
+    Calculate the dissociation constant (Kd) from the standard free energy of binding (ΔG).
 
-    Parameters
-    ----------
-    dg : float
-        Standard free energy of binding in kcal/mol.
-    temp : float, optional
-        Temperature in Kelvin. Default is 298.15 K.
+    Args:
+        dg (float): Standard free energy of binding in kcal/mol.
+        temp (float, optional): Temperature in Kelvin. Default is 298.15 K.
 
-    Returns
-    -------
-    float
-        Dissociation constant Kd in M.
+    Returns:
+        float: Dissociation constant Kd in M.
 
-    Notes
-    -----
-    The equation used to calculate Kd is:
+    Notes:
+        The equation used to calculate Kd is:
 
-    .. math::
+        $$
+        K_d = e^{\\frac{\\Delta G}{RT}}
+        $$
 
-            K_d = e^{\\frac{\\Delta G}{RT}}
-
-        where :math:`\\Delta G` is the standard free energy of binding in kcal/mol, :math:`R` is the ideal gas constant in cal/(mol K), and :math:`T` is the temperature in Kelvin.
+        where $\\Delta G$ is the standard free energy of binding,
+        $R$ is the gas constant, and $T$ is the temperature in Kelvin.
     """
     return _np.exp(dg * 1000 / ((_const.R / _const.calorie) * temp))
