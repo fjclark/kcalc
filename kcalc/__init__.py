197843ee5170a8aaf9abf129afeb45776fc35f48
finlayclark <s1629668@ed.ac.uk>
2024-08-27 11:22:32 +0100

    Add function and CLI for Kd calculation

--- kcalc/__init__.py
+++ kcalc/__init__.py
@@ -2,5 +2,9 @@
 
 from . import _version
 
+__all__ = ["calc_k"]
+
+from .conversion import calc_k
+
 __version__ = _version.get_versions()["version"]
 __all__ = ["__version__"]
--- kcalc/_cli.py
+++ kcalc/_cli.py
@@ -2,8 +2,18 @@
 
 import click
 
+from . import calc_k
+
 
 @click.command
-def main():
-    """Tutorial"""
-    pass
+@click.argument("dg", type=float)
+@click.option(
+    "--temp",
+    type=float,
+    default=298.15,
+    help="Temperature in Kelvin. Default is 298.15 K.",
+)
+def main(dg: float, temp: float):
+    """Calculate Kd from a standard free energy of binding (in kcal/mol)."""
+    kd = calc_k(dg, temp)
+    print(f"Kd = {kd:.2e} M")
